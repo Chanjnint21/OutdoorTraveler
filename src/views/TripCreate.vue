a401dfae99f05af329ec021eea39ab23fb0303b2
Chanjnint21 <limchanjnint@gmail.com>
2023-06-19 18:02:16 +0700

    views(Profile&trip): add padding to contain

--- src/views/TripCreate.vue
+++ src/views/TripCreate.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-container class="create">
+  <v-container class="MainContain">
     <v-card>
-      <v-card-title class="d-flex justify-center text-h6">Create Trip</v-card-title>
+      <v-card-title class="d-flex justify-center text-h6">Trip Card</v-card-title>
       <v-card-text>
         <v-form>
           <v-row>
@@ -9,12 +9,14 @@
               ><text-field
                 label="Title"
                 outlined
+                rounded
                 icons="mdi-alpha-t"
             /></v-col>
             <v-col cols="6">
               <text-field
                 label="Destination"
                 outlined
+                rounded
                 icons="mdi-map-marker"
               />
             </v-col>
@@ -22,17 +24,20 @@
               ><text-field
                 label="Start Date"
                 outlined
+                rounded
                 icons="mdi-calendar-start"
             /></v-col>
             <v-col cols="6"
               ><text-field
                 label="End Date"
                 outlined
+                rounded
                 icons="mdi-calendar-end"
             /></v-col>
             <v-col cols="12">
               <text-area
                 outlined
+                rounded
                 label="Details"
                 counter
               />
@@ -41,6 +46,7 @@
               <file-field
                 label="Image(s)"
                 outlined
+                rounded
                 icon="mdi-image"
               />
             </v-col>
@@ -50,6 +56,7 @@
                 outlined
                 :SelectItem="Tags"
                 multiple
+                rounded
                 small-chips
               />
             </v-col>
@@ -58,6 +65,7 @@
               <text-field
                 label="Meet Location"
                 outlined
+                rounded
                 icons="mdi-map-marker"
               />
             </v-col>
@@ -65,13 +73,14 @@
               <text-field
                 label="Leave Time"
                 outlined
+                rounded
                 icons="mdi-clock"
               />
             </v-col>
             <v-col cols="12" class="d-flex justify-center text-h6">- Requirements -</v-col>
             <v-col cols="6" class="d-flex align-center">
               <v-col cols=12>
-                <range-slider
+                <range-slider-field
                 label="Age"
                 hint="Im a hint"
                 maximum="90"
@@ -87,22 +96,37 @@
               <text-field
                 label="Cost/Person"
                 outlined
+                rounded
                 icons="mdi-currency-usd"
               />
             </v-col>
             <v-col cols="6">
-              <select-field label="National ID" outlined :SelectItem="Choice"/>
+              <select-field
+                label="National ID"
+                outlined
+                rounded
+                :SelectItem="Choice"/>
             </v-col>
             <v-col cols="6">
-              <select-field label="Phone Number" outlined :SelectItem="Choice"/>
+              <select-field
+                label="Phone Number"
+                outlined
+                rounded
+                :SelectItem="Choice"/>
             </v-col>
           </v-row>
         </v-form>
       </v-card-text>
       <v-card-action>
         <v-row class="d-flex justify-center pb-5">
-            <trip-btn class="white--text mx-3" btn-color="#478c5c" btn-label="Public"/>
-            <trip-btn class="white--text mx-3" btn-color="#013a21" btn-label="Draft"/>
+            <trip-btn
+              class="white--text mx-3"
+              btn-color="#478c5c"
+              btn-label="Public"/>
+            <trip-btn
+              class="white--text mx-3"
+              btn-color="#013a21"
+              btn-label="Draft"/>
         </v-row>
       </v-card-action>
     </v-card>
@@ -120,3 +144,16 @@ export default {
   }
 }
 </script>
+
+<style >
+@media(min-width: 1500px ){
+  .MainContain{
+    padding: 2rem 5rem;
+  }
+}
+@media(min-width: 1800px ){
+  .MainContain{
+    padding: 2rem 15rem;
+  }
+}
+</style>

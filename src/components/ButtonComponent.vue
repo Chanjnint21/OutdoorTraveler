d1066458c878c9fcfb050e7100d1020fc4b5dd9f
Chanjnint21 <limchanjnint@gmail.com>
2023-06-19 17:57:44 +0700

    Feat(Btn): delete routelink

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -6,16 +6,11 @@
   >
     <v-icon >{{ Icons }}</v-icon>
     {{ BtnLabel }}
-    <router-link
-      class="white--text text-decoration-none pl-1"
-      :to="RouterPath">
-    {{ BtnRouter }}
-    </router-link>
   </v-btn>
 </template>
 <script>
 export default {
-  name: 'ButtonComponent',
+  name: 'TripButton',
   props: {
     BtnLabel: {
       type: String
@@ -24,12 +19,6 @@ export default {
       type: String
     },
     Icons: {
-      type: String
-    },
-    RouterPath: {
-      type: String
-    },
-    BtnRouter: {
       type: String
     }
   }

366080b45536856ef0f03eb72d072a23cf29d2e7
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-06-22 17:29:06 +0700

    Adding(HomePage):Fake-JSONData

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,57 +1,70 @@
 <template>
-    <v-container class="MainContain">
-      <v-row>
-        <v-col cols="12" class="d-flex align-center pb-1">
-          <v-text-field
-            label="Search"
+  <v-container class="MainContain">
+    <v-row>
+      <!-- Search field -->
+      <v-col cols="12" class="d-flex align-center pb-1">
+        <v-text-field
+          label="Search"
+          outlined
+          rounded
+          style="width: 1000px;"
+          prepend-inner-icon="mdi-magnify"
+        />
+      </v-col>
+      <!-- Sort buttons -->
+      <v-col cols="12" class="d-flex justify-space-around mb-3">
+        <template v-for="n of 6">
+          <trip-btn
+            :key="n"
+            BtnColor="#1687A7"
+            :btn-label="Sortlabel[n]"
+            rounded
             outlined
-            rounded
-            style="width: 1000px;"
-            prepend-inner-icon="mdi-magnify"
           />
-        </v-col>
-        <v-col cols="12" class="d-flex justify-space-around mb-3">
-          <!-- <trip-btn
-            class="white--text"
-            BtnColor="#478C5C"
-            btn-label="Newest"
-            rounded
-          /> -->
-          <template v-for="n of 6">
-            <trip-btn
-              :key= 'n'
-              BtnColor="#1687A7"
-              :btn-label="Sortlabel[n]"
-              rounded
-              outlined
-            />
-          </template>
-        </v-col>
-      </v-row>
-      <!-- Vue to create a loop that will repeat the contents of the template four times, with each iteration assigned a variable named n -->
-      <template v-for="n in 10">
-        <v-col :key="n">
-          <card-component
-            height="auto"
-            class="pa-md-4 mx-lg-auto text-center"
-            elevation="8"
-            :imageUrl= "image" >
-          </card-component>
-        </v-col>
-      </template>
-
-    </v-container>
+        </template>
+      </v-col>
+    </v-row>
+    <!-- Trip cards -->
+    <template v-for="item in items">
+      <v-col :key="item.id">
+        <card-component
+          height="auto"
+          class="pa-md-4 mx-lg-auto text-center"
+          elevation="8"
+          :item="item"
+          :image-url="item.image"
+          :title="item.title"
+        />
+      </v-col>
+    </template>
+  </v-container>
 </template>
+
 <script>
+import CardComponent from '@/components/CardComponent.vue'
 import HomeImage from '@/assets/HomeImage.jpeg'
+import axios from 'axios'
+
 export default {
   name: 'HomePage',
+  components: {
+    CardComponent
+  },
   data () {
     return {
       show: false,
       page: 1,
       Sortlabel: ['', 'newest', 'oldest', 'Popular', 'Recommend', 'Price to High', 'Price to Low'],
-      image: HomeImage
+      image: HomeImage,
+      items: []
+    }
+  },
+  async created () {
+    try {
+      const response = await axios.get('http://localhost:3000/tripcards')
+      this.items = response.data
+    } catch (error) {
+      console.log(error)
     }
   }
 }

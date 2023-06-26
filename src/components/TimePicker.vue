f1cfa226610e6c3f3e9c9aef797011fbe6a1d40d
Chanjnint21 <limchanjnint@gmail.com>
2023-06-26 14:42:50 +0700

    feat(date&time picker): added

---
+++ src/components/TimePicker.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-menu
+    ref="menu"
+    v-model="menu2"
+    :close-on-content-click="false"
+    :nudge-right="40"
+    :return-value.sync="time"
+    transition="scale-transition"
+    offset-y
+    format="24hr"
+    max-width="290px"
+    min-width="290px"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-text-field
+        v-model="time"
+        label="Leave Time"
+        append-icon="mdi-clock"
+        readonly
+        rounded
+        color="#1687A7"
+        outlined
+        v-bind="attrs"
+        v-on="on"
+        dense
+      ></v-text-field>
+    </template>
+    <v-time-picker
+      color="#276678"
+      v-if="menu2"
+      v-model="time"
+      full-width
+      @click:minute="$refs.menu.save(time); savethistime(time)"
+    ></v-time-picker>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  name: 'DatePicker',
+  data: () => ({
+    date: '',
+    menu: false,
+    modal: false,
+    menu2: false
+  }),
+  methods: {
+    savethistime (time) {
+      this.$emit('time-save', time)
+    }
+  }
+}
+</script>

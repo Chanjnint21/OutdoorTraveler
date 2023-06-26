e3f84808007405f3f1b9fc22d12e45c1a70ce207
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-06-26 16:28:53 +0700

    Feat(HomePage):Adding_DeleteButton

--- src/service/index.js
+++ src/service/index.js
@@ -1,12 +1,16 @@
 import axios from 'axios'
 export const Service = {
   async getList () {
-    const data = await axios.get('http://localhost:3000/tripcards')
+    const data = await axios.get('http://localhost:3000/tripcards/')
 
     return data.data
   },
   async handleSearchQuery (q) {
     const searchData = await axios.get(`http://localhost:3000/tripcards?q=${q}`)
     return searchData.data
+  },
+  async deleteItem (id) {
+    const deleteItem = await axios.delete(`http://localhost:3000/tripcards/${id}`)
+    return deleteItem.data
   }
 }

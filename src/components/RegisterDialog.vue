76763b212ed359468f79b7d3b2a20bbfd02fcb20
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-07-26 17:44:29 +0700

    Fixing(RegisterDialogForm):version2

--- src/components/RegisterDialog.vue
+++ src/components/RegisterDialog.vue
@@ -1,5 +1,4 @@
-<template>
-  <v-row>
+  <template>
     <v-dialog
       v-model="dialog"
       persistent
@@ -21,13 +20,13 @@
           <span class="text-h5 mb-5">Registration Form</span>
         </v-card-title>
         <v-card-text>
-          <v-form ref='form' v-model="registerCard">
+          <v-form ref='form' v-model="isFormComplete">
             <v-row>
               <v-col
                 cols="12"
               >
-                <text-field
-                  v-model="subForm.userName"
+              <text-field
+                v-model="regisForm.userName"
                   label="Username"
                   required
                   readonly
@@ -39,7 +38,7 @@
                 sm="6"
               >
                 <text-field
-                  v-model="subForm.firstName"
+                  v-model="regisForm.firstName"
                   label="First name"
                   required
                   :rules="[rules.createrule]"
@@ -51,7 +50,7 @@
                 sm="6"
               >
                 <text-field
-                  v-model="subForm.lastName"
+                  v-model="regisForm.lastName"
                   label="Last name"
                   :rules="[rules.createrule]"
 
@@ -62,7 +61,8 @@
                 sm="6"
               >
                 <text-field
-                  v-model="subForm.phoneNumber"
+                  type="number"
+                  v-model="regisForm.phoneNumber"
                   label="Phone number"
                   required
                   :rules="[rules.createrule]"
@@ -73,10 +73,11 @@
                 sm="6"
               >
                 <text-field
-                  v-model="subForm.age"
+                  v-model="regisForm.age"
+                  type="number"
                   label="Age"
                   required
-                  :rules="[rules.createrule]"
+                  :rules="[rules.ageRule]"
 
                 />
               </v-col>
@@ -90,85 +91,170 @@
                   icon="mdi-image"
                   hint="Click to upload your national ID Card"
                   persistent-hint
+                  :rules="[]"
                 />
               </v-col>
             </v-row>
           </v-form>
         </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="#276678"
-            text
-            @click="dialog = false"
-          >
-            Cancel
-          </v-btn>
-          <v-btn
-            color="#1687A7"
-            text
-            @click="submit(); dialog = false"
-            :disabled='!registerCard'
-          >
-            submit
-          </v-btn>
-        </v-card-actions>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="#276678"
+                text
+                @click="dialog = false"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                color="#1687A7"
+                text
+                @click="submit(); dialog = false"
+                :disabled='!isFormComplete '
+              > Submit
+              </v-btn>
+            </v-card-actions>
       </v-card>
     </v-dialog>
-  </v-row>
-</template>
+  </template>
 
 <script>
 import { Service } from '@/service/index.js'
 
 export default {
   props: {
-    this_card: {
+    this_id: {
       type: String,
-      require: true
+      required: true
     }
   },
   data: () => ({
-    registerCard: true,
+    isFormComplete: false,
+    isAgeValid: false,
     dialog: false,
+    validAge: [],
     crrUser: JSON.parse(localStorage.getItem('authUser')),
-    Username: '',
-    rules: {
-      createrule: value => !!value || 'field required'
-    },
-    subForm: {
+    // rules: {
+    //   createrule: value => !!value || 'field required',
+    //   ageRule: value => {
+    //     const tripData = Service.thisIdData(this.this_id)
+    //     if (!value || isNaN(value)) {
+    //       return "your age isn't valid !"
+    //     }
+    //     const age = parseInt(value)
+    //     if (age >= tripData.requirement.age[0] && age <= tripData.requirement.age[1]) {
+    //       return true
+    //     } else {
+    //       return false
+    //     }
+    //   }
+    // },
+    regisForm: {
       id: '',
       user_id: '',
       userName: '',
       firstName: '',
-      lastName: '',
       age: '',
+      lastName: '',
       phoneNumber: '',
       card_id: '',
       reg_date: new Date().toLocaleDateString(),
       reg_time: new Date().toLocaleTimeString('en-US', { hour12: false })
     }
   }),
-  watch: {
-    dialog (newVal) {
-      if (newVal) {
-        this.subForm.userName = this.crrUser[0].name
-        this.subForm.user_id = this.crrUser[0].id
-        this.subForm.card_id = this.this_card
-        this.subForm.firstName = this.crrUser[0].firstName
-        this.subForm.lastName = this.crrUser[0].lastName
-        this.subForm.phoneNumber = this.crrUser[0].phone
+  computed: {
+    rules () {
+      return {
+        createrule: this.validateCreateRule,
+        ageRule: value => {
+          const age = parseInt(value)
+          if (age >= this.validAge[0] && age <= this.validAge[1]) {
+            return true
+          } else {
+            return false
+          }
+        }
       }
     }
   },
+  watch: {
+    dialog (newVal) {
+      if (newVal) {
+        this.regisForm.userName = this.crrUser[0].name
+        this.regisForm.user_id = this.crrUser[0].id
+        this.regisForm.card_id = this.this_id
+        this.regisForm.firstName = this.crrUser[0].firstName
+        this.regisForm.lastName = this.crrUser[0].lastName
+        this.regisForm.phoneNumber = this.crrUser[0].phone
+      }
+    },
+    age: {
+      immediate: true,
+      handler (newVal) {
+        this.ageRule = newVal
+      }
+    }
+
+  },
   methods: {
     async submit () {
-      await Service.submitRegister(this.subForm)
-      this.$emit('register', true)
+      if (this.isFormComplete) {
+        try {
+          await Service.submitRegister(this.regisForm)
+          this.clearForm()
+          this.$emit('register', true)
+        } catch (e) {
+          console.log(e)
+        }
+      }
+    },
+    async validateAge () {
+      try {
+        const tripData = await Service.thisIdData(this.this_id)
+        this.validAge = tripData.requirement.age
+        console.log(this.validAge)
+        // const age = parseInt(value)
+        // if (!isNaN(age) && age >= tripData.requirement.age[0] && age <= tripData.requirement.age[1]) {
+        //   return 'Your age is valid!'
+        // } else {
+        //   return 'Age must be within the valid range.'
+        // }
+      } catch (e) {
+        console.log(e)
+        return 'Failed to fetch trip data.'
+      }
     },
     validate () {
-      this.$refs.form.validate()
+      this.isFormComplete = this.$refs.form.validate()
+    },
+    validateCreateRule (value) {
+      return !!value || 'field required'
+    },
+
+    // validateAge (value) {
+    //   const tripData = JSON.parse(this.this_card)
+    //   const age = parseInt(value)
+
+    //   if (!isNaN(age) && age >= tripData.requirement.age[0] && age <= tripData.requirement.age[1]) {
+    //     this.isAgeValid = true
+    //     console.log('hey its ur first step')
+    //     return true // Return true when age is valid
+    //   } else {
+    //     this.isAgeValid = false
+    //     return 'Age must be within the valid range.' // Return error message when age is not valid
+    //   }
+    // },
+    clearForm () {
+      this.userName = ''
+      this.firstName = ''
+      this.age = ''
+      this.lastName = ''
+      this.phoneNumber = ''
+      this.card_id = ''
     }
+  },
+  mounted () {
+    this.validateAge()
   }
 }
 </script>

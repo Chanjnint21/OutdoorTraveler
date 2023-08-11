281532c0c01c5bd20d908461b6a0d5f4e8701d96
Chanjnint21 <limchanjnint@gmail.com>
2023-08-11 15:18:00 +0700

    added Image and name to Follwer and following

--- src/views/Profile/components/FollowComponent.vue
+++ src/views/Profile/components/FollowComponent.vue
@@ -1,48 +1,56 @@
 <template>
-<v-row justify="center">
-  <v-dialog
-  v-model="dialog"
-  scrollable
-  max-width="300px"
-  v-bind="$attrs"
-  v-on="$listeners"
->
-<template v-slot:activator="{ on, attrs }">
-  <v-col cols="12" class="d-flex">
-    <p
-      v-bind="attrs"
-      v-on="on">{{ followingCount }} <span class="text--disabled mr-2"> following</span>
-    </p>
-  </v-col>
-</template>
-<v-card>
-  <v-card-title>
-    <span class="text-h5 mb-5">Your Following</span>
-  </v-card-title>
-  <v-divider></v-divider>
-  <v-list-item v-for="list in followingList" :key="list.id">
-    <v-list-item-content>{{list}}</v-list-item-content>
-  </v-list-item>
-  <v-card-actions>
-    <v-btn
-    color="blue darken-1"
-    text
-    @click="dialog = false">
-    Close
-</v-btn>
-  <v-btn
-    color="blue darken-1"
-    text
-    @click="dialog = false">
-    Okay
-</v-btn>
-  </v-card-actions>
-</v-card>
-</v-dialog>
-</v-row>
+  <v-row justify="center">
+    <v-dialog
+    v-model="dialog"
+    scrollable
+    max-width="400px"
+    v-bind="$attrs"
+    v-on="$listeners"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-col cols="12" class="d-flex">
+          <p
+            v-bind="attrs"
+            v-on="on">{{ followingCount }} <span class="text--disabled mr-2"> following</span>
+          </p>
+        </v-col>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">Your Following</span>
+        </v-card-title>
+        <v-divider></v-divider>
+        <!-- <v-list-item v-for="list in followingList" :key="list.id">
+          <v-list-item-content>{{list}}</v-list-item-content>
+        </v-list-item> -->
+        <v-list-item v-for="(list, i) in followingList" :key="list">
+        <v-list-item-content class="d-flex justify-space-between">
+          <div>
+            <v-avatar size="40" class="mx-3">
+              <v-img :src="followingImg[i]" />
+            </v-avatar>
+            <router-link class="text-decoration-none black--text" :to="`/user/profile/${followingList[i]}`" >{{ list }}</router-link>
+          </div>
+        </v-list-item-content>
+        </v-list-item>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+          color="blue darken-1"
+          text
+          @click="dialog = false"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
 </template>
 
 <script>
+import { storage } from '../../../firebase'
+import { ref, getDownloadURL } from 'firebase/storage'
 import { Service } from '@/service/index.js'
 export default {
   name: 'FollowDialog',
@@ -56,20 +64,28 @@ export default {
     dialog: false,
     followingList: [],
     followerList: [],
-    crrUser: JSON.parse(localStorage.getItem('authUser'))[0]
-
+    crrUser: JSON.parse(localStorage.getItem('authUser'))[0],
+    followingImg: []
   }),
   methods: {
     followingData (data) {
       for (let i = 0; i < data.length; i++) {
-        this.followingList.push(data[i].following)
-        console.log(this.followingList)
+        this.onGetFollowingName(data[i].following)
       }
+    },
+    async onGetFollowingName (data) {
+      const userName = await Service.getUser(data)
+      const path = `profile/${userName.image}`
+      getDownloadURL(ref(storage, path)).then(
+        (downLoadUrl) => {
+          this.followingImg.push(downLoadUrl)
+          this.followingList.push(userName.name)
+        }
+      )
     }
   },
   async mounted () {
     const following = await Service.followingList(this.crrUser.id)
-    console.log('following', this.crrUser.firstName)
     this.followingData(following)
   }
 }
--- src/views/Profile/components/FollowerComponent.vue
+++ src/views/Profile/components/FollowerComponent.vue
@@ -3,45 +3,52 @@
     <v-dialog
     v-model="dialog"
     scrollable
-    max-width="300px"
+    max-width="400px"
     v-bind="$attrs"
-    v-on="$listeners">
-  <template v-slot:activator="{ on, attrs }">
-    <v-col cols="12" class="d-flex">
-      <p
-        v-bind="attrs"
-        v-on="on"> {{ followerCount }}
-        <span class="text--disabled mr-2">follower</span>
-      </p>
-    </v-col>
-  </template>
-  <v-card>
-    <v-card-title>
-      {{ follower }}
-    </v-card-title>
-    <v-divider></v-divider>
-    <v-list-item v-for="list in followerList" :key="list.user_id">
-      <v-list-item-content>{{list}}</v-list-item-content>
-    </v-list-item>
-    <v-card-actions>
-      <v-btn
-      color="blue darken-1"
-      text
-      @click="dialog = false">
-      Close
-  </v-btn>
-    <v-btn
-      color="blue darken-1"
-      text
-      @click="dialog = false">
-      Okay
-  </v-btn>
-    </v-card-actions>
-  </v-card>
+    v-on="$listeners"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-col cols="12" class="d-flex">
+        <p
+          v-bind="attrs"
+          v-on="on"> {{ followerCount }}
+          <span class="text--disabled mr-2">follower</span>
+        </p>
+      </v-col>
+    </template>
+    <v-card>
+      <v-card-title>
+        {{ follower }}
+      </v-card-title>
+      <v-divider></v-divider>
+      <v-list-item v-for="(list, i) in followerList" :key="list">
+        <v-list-item-content class="d-flex justify-space-between">
+          <div>
+            <v-avatar size="40" class="mx-3">
+              <v-img :src="followerImg[i]" />
+            </v-avatar>
+            <router-link class="text-decoration-none black--text" :to="`/user/profile/${followerList[i]}`" >{{ list }}</router-link>
+          </div>
+        </v-list-item-content>
+      </v-list-item>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="blue darken-1"
+          text
+          @click="dialog = false"
+        >
+          Close
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </v-dialog>
   </v-row>
-  </template>
+</template>
+
 <script>
+import { storage } from '../../../firebase'
+import { ref, getDownloadURL } from 'firebase/storage'
 import { Service } from '@/service/index.js'
 export default {
   name: 'FollowerDialog',
@@ -55,14 +62,24 @@ export default {
     dialog: false,
     followerList: [],
     crrUser: JSON.parse(localStorage.getItem('authUser'))[0],
-    follower: 'Your Follower'
+    follower: 'Your Follower',
+    followerImg: []
   }),
   methods: {
     followerData (data) {
       for (let i = 0; i < data.length; i++) {
-        this.followerList.push(data[i].following)
+        this.onGetFollowerName(data[i].user_id)
       }
-      console.log('followerList', this.followerList)
+    },
+    async onGetFollowerName (data) {
+      const userName = await Service.getUser(data)
+      const path = `profile/${userName.image}`
+      getDownloadURL(ref(storage, path)).then(
+        (downLoadUrl) => {
+          this.followerImg.push(downLoadUrl)
+          this.followerList.push(userName.name)
+        }
+      )
     }
   },
   async mounted () {
